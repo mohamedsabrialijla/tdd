72bd02cc75a2cfb7243c1593eae0529eaedcf5c4
Mohammed S Shurrab <m.sh@unitone.ps>
2016-08-29 22:34:20 +0300

    Update ShipmentTest.php

--- ShipmentTest.php
+++ ShipmentTest.php
@@ -3,108 +3,34 @@ use PHPUnit\Framework\TestCase;
 
 class ShipmentTest extends TestCase
 {
-
     /** @test */
     public function calculate_shipment_per_weight_for_each_product()
     {
-        $product = new Product(5);
-
+        $product = new Product();
+        $product->setWeight(6);
         $shippingCalculator = new ShipmentCalculator();
-
-        $this->assertEquals(18, $shippingCalculator->calculate($product));
-
-        $product->weight = 1;
-        $this->assertEquals(10, $shippingCalculator->calculate($product));
+        $this->assertEquals(12, $shippingCalculator->calculateProduct($product));
     }
-
     /** @test */
-    public function product_must_have_weight(){
-        $product = new Product(4);
-        $this->assertFalse(false, $product->checkWeight() );
-
-        $this->assertTrue(true, $product->checkWeight() );
-    }
-
-    /** @test */
-    public function shipment_price_cantot_be_less_than_10(){
-        $shippingCalculator = new ShipmentCalculator();
-
-        $this->assertEquals(10, $shippingCalculator->minmumPrice() );
-    }
-
-    /** @test */
-    public function get_total_weigth(){
-        $product1 = new Product(1);
-        $product2 = new Product(2);
-
+    public function calculate_the_total_shipment_price_for_the_whole_cart(){
+        $product1 = new Product();
+        $product1->setWeight(4);
+        $product2 = new Product();
+        $product2->setWeight(7);
         $cart = new Cart();
         $cart->addToCart($product1);
         $cart->addToCart($product2);
-
-        $total = $cart->getTotalWeight();
-        $this->assertEquals(3, $total );
-
         $shippingCalculator = new ShipmentCalculator();
-
-        $this->assertEquals(14, $shippingCalculator->calculateCart($cart));
+        $this->assertEquals(22, $shippingCalculator->calculateCart($cart));
     }
-}
-
-class Product {
-    public $weight;
-
-    public function __construct($weight) {
-        $this->weight = $weight;
+    
+    /** @test */
+    public function shipment_price_cannot_be_less_than_10(){
+        $product = new Product();
+        $product->setWeight(4);
+        $shippingCalculator = new ShipmentCalculator();
+        $this->assertEquals(10, $shippingCalculator->calculateProduct($product));
     }
-
-    public function checkWeight(){
-        if ($this->weight > 0) {
-            return true;
-        }
-        return false;
-    }
-}
-
-class Cart {
-    public  $products = [];
-
-    public function addToCart($product)   {
-        $this->products[] = $product;
-    }
-    public function getTotalWeight()   {
-        $total = 0;
-        foreach ($this->products as $product) {
-            $total += $product->weight;
-        }
-        return $total;
-    }
-}
-
-class ShipmentCalculator {
-
-    public $minmumPricePerShipment = 10;
-    public $shipmentRatePer1Kg = 2;
-
-    public function calculate(Product $product)   {
-        if($product->weight > 1 ) {
-            $newWeight = $product->weight - 1;
-            $shipmentPrice = ($this->minmumPricePerShipment) + ($newWeight * $this->shipmentRatePer1Kg);
-            return $shipmentPrice;
-        }
-        else
-            return $this->minmumPricePerShipment;
-
-    }
-
-    public function calculateCart(Cart $cart)   {
-        $shipmentPrice = ($this->minmumPricePerShipment) + (($cart->getTotalWeight()-1) * $this->shipmentRatePer1Kg);
-        return $shipmentPrice;
-    }
-
-    public function minmumPrice(){
-        return $this->minmumPricePerShipment;
-    }
-
 }
 
 ?>
