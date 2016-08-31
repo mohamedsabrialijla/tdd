05a15d0c31e9f859703937dd5049296357998ff2
Mohammed Jalal Albanna <eng.banna1@gmail.com>
2016-08-31 14:07:08 +0300

    Update VendorTest.php

--- VendorTest.php
+++ VendorTest.php
@@ -5,86 +5,94 @@ use PHPUnit\Framework\TestCase;
 
 class VendorTest extends TestCase
 {
+
+
     /** @test */
     public function vendor_can_only_own_single_store()
     {
         $vendor = new Vendor();
 
-        $store_count = $vendor->get_store_count();
+        /** Set store1 to the vendor must be success  */
+        $this->assertEquals(true, $vendor->setStore(1));
 
-        $this->assertEquals(1, $store_count);
+        /** if the vendor have store must not able to accept another store  */
+        $this->assertEquals(false, $vendor->setStore(2));
 
-    }
-
-    /** @test */
-    public function vendor_can_only_own_single_store2()
-    {
-        $vendor = new Vendor();
-
-        $this->assertEquals(true, $vendor->addStore(1));
-        $this->assertEquals(false, $vendor->addStore(2));
+        /** When get the vendor store must return the first store, that mean the vendor has only one store  */
+        $this->assertEquals(1, $vendor->getStore());
 
     }
 
     /** @test */
     public function vendor_can_add_other_manager_to_his_store()
     {
+        $store1 = new Store();
+        $store1->name = 'Store1';
 
-        $manager = new User();
-        $manager->name = 'Mohammed';
+        $store2 = new Store();
+        $store2->name = 'Store2';
+
+        $manager1 = new User();
+        $manager1->name = 'Mohammed';
 
         $manager2 = new User();
         $manager2->name = 'Ahmed';
 
-        $store = new Store();
-        $vendor = new Vendor();
-        $vendor->name = 'VVendor';
+        /** get $currentUser */
+        $currentUser = Auth::user()->id;
+        /** set current user as a Vendor to store1 */
+        $store1->setVendor($currentUser);
 
-        $vendor2 = new Vendor();
-        $vendor2->name = 'mVendor2';
+        /** Add manager1 to Store1 (store1 is  currentUser store) */
+        $res1 = $store1->addManager($manager1);
 
-        $store->setVendor($vendor);
+        /** Add manager2 to Store1 (store1 is  currentUser store) */
+        $res2 = $store1->addManager($manager2);
 
-        $res1 = $store->addManager($vendor,$manager);
-        $res2 = $store->addManager($vendor,$manager2);
+        /** Vendor can not add managers to another store
+         * test to Add manager2 to Store2 (store2 isn't  currentUser store) */
+        $res3 = $store2->addManager($manager2);
 
-        $res3 = $store->addManager($vendor2,$manager2);
-
-        $managers = count($store->getManagers());
+        /** get a;; managers in Store1 must return 2 */
+        $managers = count($store1->getManagers());
 
         $this->assertEquals(true, $res1);
         $this->assertEquals(true, $res2);
-        $this->assertEquals(2, $managers);
         $this->assertEquals(false, $res3);
-
+        $this->assertEquals(2, $managers);
     }
 
     /** @test */
     public function vendor_and_manager_can_add_product_to_store()
     {
 
-        $product = new Product();
-        $product->title = 'My product';
-        $product->price = 20;
+        /** Add new Store => 'Store1' */
+        $store1 = new Store();
+        $store1->name = 'Store1';
+
+        /** Add current user as a vendor on Store1 */
+        $currentUser = Auth::user()->id;
+        $store1->setVendor($currentUser);
+
+        /** Vendor add $product1 to $store1*/
+        $product1 = new Product();
+        $product1->id = 4;
+
+        $this->assertEquals(true, $store1->addProduct($product1));
 
 
-        $store = new Store(1);
+        /** Add  Manager1 as a manager to Store1 */
+        $manager1 = new User();
+        $manager1->id = 2;
+        $manager1->name = 'Mohammed';
+        $addManagerResult = $store1->addManager($manager1->id);
 
-        $vendor = new Vendor();
-        $vendor->name = 'VVendor';
-        $store->setVendor($vendor);
+        /** change the current user to $manager1 */
+        //todo I must change the current user id to 2
+        //Auth::loginUsingId(2);
+        $this->assertEquals(true, $store1->addProduct($product1));
 
-        $manager = new Manager();
-        $manager->name = 'Mohammed';
-
-        $user = new User();
-
-        $res1 = $store->addManager($vendor,$manager);
-
-
-        $this->assertEquals(true, $store->addProduct($product, $vendor));
-        $this->assertEquals(true, $store->addProduct($product, $manager));
-        $this->assertEquals(false, $store->addProduct($product, $user));
+        $this->assertEquals(2, count($store1->getProducts()));
     }
 
 
